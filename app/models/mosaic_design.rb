054673693eb2ab06626f670015082656ab53a27f
Tatsumi_Baba <tatsumi1680@icloud.com>
2026-07-31 20:24:12 +0900

    モザイクアートのイラストデータ追加とRubocop修正

--- app/models/mosaic_design.rb
+++ app/models/mosaic_design.rb
@@ -2,5 +2,5 @@ class MosaicDesign < ApplicationRecord
   has_many :design_pieces, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
-  validates :area_size_x, :area_size_y, presence: true, numericality: { greater_than: 0}
+  validates :area_size_x, :area_size_y, presence: true, numericality: { greater_than: 0 }
 end
