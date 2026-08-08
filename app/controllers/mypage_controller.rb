1a6a31ffee0e276855f33d910baa691942b0bf72
Tatsumi_Baba <tatsumi1680@icloud.com>
2026-08-08 14:49:13 +0900

    ログイン必須

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,7 +1,20 @@
 class MypageController < ApplicationController
   include MosaicGridAssignable
 
+  # ログインしていない場合はログインページにリダイレクト（Deviseのメソッド）
+  before_action :authenticate_user!
+
   def index
+    # マイページにアクセスしたユーザーのグリッドを割り当て
     assign_mosaic_grid!(current_user)
+    # マイページにアクセスしたユーザーのストレッチログを取得
+    @stretch_logs = current_user.stretch_logs
+                                .includes(:stretch)
+                                 # ストレッチログを作成日時で降順に並べ替え
+                                 .order(performed_at: :desc)
+                                 # 最新の5件を取得
+                                 .limit(5)
+    # マイページにアクセスしたユーザーのストレッチ連続日数を取得
+    @streak_days = current_user.current_streak_days
   end
 end
