7049b50a0ebc3bec49c6bc61ccbfa7ebe30ce66e
Tatsumi_Baba <tatsumi1680@icloud.com>
2026-08-31 15:50:48 +0900

    未ログイン時のユーザーフローを調整

--- app/controllers/stretches_controller.rb
+++ app/controllers/stretches_controller.rb
@@ -1,11 +1,12 @@
 class StretchesController < ApplicationController
+  # ストレッチ選択ページ
   def index
     if params[:body_part].present?
       @stretches = Stretch.where(body_part: params[:body_part])
-      # ↑ ビュー実装は次の「ストレッチ選択」issueで対応
     end
   end
 
+  # ストレッチ実施ページ
   def show
     @stretch = Stretch.includes(:stretch_steps).find(params[:id])
   end
--- app/javascript/controllers/stretch_log_dialog_controller.js
+++ app/javascript/controllers/stretch_log_dialog_controller.js
@@ -3,29 +3,42 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="stretch-log-dialog"
 export default class extends Controller {
   static targets = ["completeDialog", "abortDialog"]
-  static values  = { stretchId: Number }
+  static values  = { stretchId: Number, signedIn: Boolean }
 
+  // コンテキストが読み込まれたらカウントダウンタイマーが完了したらストレッチ実施完了ダイアログを表示するイベントリスナーを追加
   connect() {
     window.addEventListener("countdown-timer:complete", this.openCompleteDialogFromEvent)
   }
 
+  // コンテキストが破棄されたらカウントダウンタイマーが完了したらストレッチ実施完了ダイアログを表示するイベントリスナーを削除
   disconnect() {
     window.removeEventListener("countdown-timer:complete", this.openCompleteDialogFromEvent)
   }
 
+  // カウントダウンタイマーが完了したらストレッチ実施完了ダイアログを表示するメソッド
   openCompleteDialogFromEvent = () => {
     this.completeDialogTarget.showModal()
   }
 
+  // ストレッチ実施中止確認ダイアログを表示するメソッド
   openAbortConfirm() {
     this.abortDialogTarget.showModal()
   }
 
+  // ストレッチ実施中止確認ダイアログを閉じるメソッド
   closeAbortConfirm() {
     this.abortDialogTarget.close()
   }
 
+  // ストレッチ実施記録を保存し、マイページにリダイレクトするメソッド
   recordAndRedirect() {
+    // ログインしていない場合は部位選択ページにリダイレクト
+    if (!this.signedInValue) {
+      window.Turbo.visit("/stretches")
+      return
+    }
+
+    // ログインしている場合はストレッチ実施記録を保存し、マイページにリダイレクト
     fetch(`/stretch_logs?stretch_id=${this.stretchIdValue}`, {
       method: "POST",
       headers: {
